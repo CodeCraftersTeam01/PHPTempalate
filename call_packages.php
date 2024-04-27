69da7c7b5aa38cbe01017bb967b1c6f92972c0e3
Arjuna Lanang <wardilanang46@gmail.com>
2024-04-27 17:29:56 +0700

    first Commit

---
+++ call_packages.php
@@ -0,0 +1,8 @@
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<link rel="stylesheet" href="asset/design/bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" href="asset/animation/animate.css/animate.min.css">
+<link rel="stylesheet" href="asset/animation/aos/aos/dist/aos.css">
+<script src="asset/design/bootstrap/dist/js/bootstrap.js"></script>
+<script src="asset/animation/aos/aos/dist/aos.js"></script>
+
